4cce48b926bfae21a2149e112818525d03951a8d
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-10 21:24:34 +0200

    all

--- core/settings.py
+++ core/settings.py
@@ -120,15 +120,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-"""
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'"""
