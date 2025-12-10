da4b9387f5c54fd9b6ef3a3fd9663d92565eaf63
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-10 21:21:56 +0200

    all files

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = models.CharField(max_length=120, null=True, blank=True)
+  description = models.TextField(max_length=500, null=True, blank=True)
+  date_of_birth = models.DateField(null=True, blank=True)
+  phone = models.CharField(max_length=13, null=True, blank=True)
+  instagram_user = models.CharField(max_length=100, null=True, blank=True)
+  address = models.CharField(max_length=200, null=True, blank=True)
+  clints = models.IntegerField(null=True, blank=True)
+  hour_worked = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+  def __str__(self):
+    return f'{self.id} - user id{self.user.id}'
