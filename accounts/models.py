a99b82add5de62d9399c52d57718325e77dd87ad
khaledgawesh <khaled.gawesh@gmail.com>
2025-12-11 19:20:25 +0200

    create new profile when post

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -15,5 +17,11 @@ class Profile(models.Model):
   hour_worked = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
   image = models.ImageField(upload_to="profile", null=True, blank=True)
 
+      
   def __str__(self):
     return f'{self.id} - user id{self.user.id}'
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
